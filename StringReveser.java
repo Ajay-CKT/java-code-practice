b6236a79c684203ae014ea14fca166224068ef68
Ajay-CKT <ajayckt917@gmail.com>
2024-08-09 13:47:04 +0530

    folder change

--- StringReveser.java
+++ StringReveser.java
@@ -1,18 +1,16 @@
 import java.util.Scanner;
 
-public class StringReverse {
+public class StringReveser {
     static String reverseString(String str) {
         if (str.isEmpty())
             return str;
-        return reverseString(str.substring(1)) + str.charAt(0);
+        return reverseString(str.substring(1) + str.charAt(0));
     }
 
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
         String str = userInput.next();
+        System.out.println(reverseString(str));
         userInput.close();
-        String result = reverseString(str);
-        System.out.println(result);
-
     }
 }
